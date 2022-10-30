7c37af0ce8da0b25a6da85e90d7d62b9663e297b
Shilong Zhao <zhao.s.long@gmail.com>
2022-10-30 11:34:00 +0100

    add logging

--- src/main/kotlin/com/codeora/coroutine/PartnerService.kt
+++ src/main/kotlin/com/codeora/coroutine/PartnerService.kt
@@ -7,17 +7,22 @@ import org.springframework.stereotype.Service
 @Service
 class PartnerService(
     private val partnerSpec: PartnerSpec
-) {
+): Logging {
     suspend fun getCoins(): List<Coin> {
+        logger.info("start getting coins")
         delay(1000)
         val coinsMap = Coin.values().associateBy { it.code }
         return partnerSpec.getCoins().map {
             coinsMap.getValue(it.code)
+        }.also {
+            logger.info("got coins")
         }
     }
 
     suspend fun getCustomers(): List<Customer> {
+        logger.info("start getting customers")
         delay(1000)
         return partnerSpec.getCustomers().map { Customer(it.id, it.email) }
+            .also { "got customers" }
     }
 }
